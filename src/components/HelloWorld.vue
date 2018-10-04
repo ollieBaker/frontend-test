7e569efc2c086f0bd666cb037929df78c0eab38d
Ollie Baker <oliverjohnbaker@gmail.com>
2018-10-04 20:08:15 +0100

    quick editable list using contenteditable

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,34 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <v-list
+          v-for="user in users"
+          :key="user.id"
+          >
+            <v-list-tile-content @click="beginEdit(user.id)">
+              <v-list-tile-sub-title>{{ user.id }}</v-list-tile-sub-title>
+              <v-list-tile-sub-title 
+                v-text="user.name"
+                contenteditable="true" 
+                @blur="e => editUser(e.target, 'name', user.id)">
+              </v-list-tile-sub-title>
+              <v-list-tile-sub-title 
+                contenteditable="true" 
+                @blur="e => editUser(e.target, 'email', user.id)">
+                {{ user.email }} 
+              </v-list-tile-sub-title>
+              <v-list-tile-sub-title 
+                contenteditable="true" 
+                @blur="e => editUser(e.target, 'company.name', user.id)">
+                {{ user.company.name }}
+              </v-list-tile-sub-title>
+              <v-list-tile-sub-title v-if="editing === user.id" @click="e => updateUser(e, user.id)">Send</v-list-tile-sub-title>
+            </v-list-tile-content>
+        </v-list>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
 </template>
 
 <script>
@@ -35,9 +36,53 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      users: [],
+      editing: null
+    };
+  },
+  created() {
+    this.getUsers();
+  },
+  methods: {
+    editUser(target, key, id) {
+      const user = this.users.find(user => user.id == id);
+      this.$set(user, key, target.innerText);
+    },
+    beginEdit(id) {
+      this.editing = id;
+    },
+    edit(val, target) {},
+    getUsers() {
+      this.$http
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then(response => (this.users = response.data));
+    },
+    updateUser(e, id) {
+      e.stopPropagation();
+
+      const userIndex = this.users.findIndex(user => user.id == id);
+      this.editing = null;
+      this.$http
+        .put(
+          `https://jsonplaceholder.typicode.com/users/${id}`,
+          this.users[userIndex]
+        )
+        .then(response => {
+          console.log(response);
+          this.$set(this.users, userIndex, response.data);
+        })
+        .catch(err => {
+          console.log(err);
+          this.editing = id;
+        });
+    }
   }
 };
 </script>
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
